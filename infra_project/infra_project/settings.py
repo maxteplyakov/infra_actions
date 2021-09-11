ca05909e1c1bb559b0fcc9ee4a6a0194a3fcf6a7
Max Teplyakov <maxteplyakov@gmail.com>
2021-09-11 15:28:33 +0700

    fix allowed hosts in settings.py

--- infra_project/infra_project/settings.py
+++ infra_project/infra_project/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'k9gwcfq01xszx2(_4e#gc3u$rt=)y6ivx=$wt+5&-^ktk4dg4e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '178.154.195.6',
+    'mt-projects.ru',
+    'www.mt-projects.ru'
+]
 
 
 # Application definition
