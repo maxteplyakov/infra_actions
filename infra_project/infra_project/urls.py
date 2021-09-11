a3e1f3de352e8018f611b617a0c47e73afc3d618
Max Teplyakov <maxteplyakov@gmail.com>
2021-09-11 14:19:14 +0700

    pep8

--- infra_project/infra_project/urls.py
+++ infra_project/infra_project/urls.py
@@ -1,6 +1,6 @@
+from django.contrib import admin
 from django.urls import path
 from django.urls import include
-from django.contrib import admin
 
 urlpatterns = [
     path('', include('infra_app.urls', namespace='infra_app')),
